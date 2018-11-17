a145c37387f9b68027cbf55fb93f106cb9795d92
smangham <mangham@gmail.com>
2018-11-17 15:30:14 +0000

    Added percentage

--- question1.py
+++ question1.py
@@ -31,6 +31,18 @@ def simple_histogram(source, column_name, title, top_n=10):
     fig.tight_layout()
     fig.savefig(column_name, transparent=True)
 
+    fig, ax = plt.subplots(1, 1)
+    rects1 = ax.bar(result['Category'].head(top_n), result['Count'].head(top_n)*100/len(source))
+
+    ax.set_ylabel('Percentage')
+    ax.set_title(title)
+
+    labels = ['\n'.join(wrap(label, 20)) for label in result['Category'].head(top_n)]
+    ax.set_xticklabels(labels, size=8, rotation=90)
+
+    fig.tight_layout()
+    fig.savefig(column_name+'_perc', transparent=True)
+
 simple_histogram(gladys, 'client_group', 'Top 10 client groups')
 simple_histogram(gladys, 'project_nature', 'Top 10 project natures')
 simple_histogram(gladys, 'member_service', 'Top 10 member services')
